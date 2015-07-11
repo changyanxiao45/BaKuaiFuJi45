3da733251a19872faa5cd40a26b067ff42bda6fc
changyanxiao45 <xiaohu45@126.com>
2015-07-11 17:26:53 +0800

    Update BinaryTree.cpp

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -9,6 +9,43 @@ typedef struct BinaryTree
   BinaryTree *pRChild;
 }BTNode, *pBTNode;
 
+/*binary tree operation*/
+
+bool Visit(pBTNode &pNode);
+
+bool CreateBinaryTree(pBTNode &pNode);
+void PrevOrderTraverse(pBTNode &pNode);
+void MiddOrderTraverse(pBTNode &pNode);
+void PostOrderTraverse(pBTNode &pNode);
+
+int GetAllNodeNum(pBTNode &pNode);
+int GetBTDepthNum(pBTNode &pNode);
+int GetLeafNodeNum(pBTNode &pNode);
+int GetKthLevelNodeNum(pBTNode &pNode, int k);
+
+int main()
+{
+  //ABC##DE#G##F###
+  pBTNode pNode;
+  cout<<"Input the Binary Tree node:";
+  CreateBinaryTree(pNode);
+  return 0;
+}
+
+bool Visit(pBTNode &pNode)
+{
+  if(NULL != pNode)
+  {
+    cout<<"null";
+    return false;
+  }
+  else
+  {
+    cout<<pNode->data;
+    return true;
+  }
+}
+
 bool CreateBinaryTree(pBTNode &pNode)
 {
   char ch;
@@ -27,11 +64,57 @@ bool CreateBinaryTree(pBTNode &pNode)
   return true;
 }
 
-int main()
+void PrevOrderTraverse(pBTNode &pNode)
 {
-  //ABC##DE#G##F###
-  pBTNode pNode;
-  cout<<"Input the Binary Tree node:";
-  CreateBinaryTree(pNode);
-  return 0;
+  if(pNode)
+    return;
+  Visit(NULL == pNode);
+  PrevOrderTraverse(pNode->pLChild);
+  PrevOrderTraverse(pNode->pRChild);
+}
+
+void MiddOrderTraverse(pBTNode &pNode)
+{
+  if(NULL == pNode)
+    return;
+  PrevOrderTraverse(pNode->pLChild);
+  Visit(pNode);
+  PrevOrderTraverse(pNode->pRChild);
+}
+
+void PostOrderTraverse(pBTNode &pNode)
+{
+  if(NULL == pNode)
+    return;
+  PrevOrderTraverse(pNode->pLChild);
+  PrevOrderTraverse(pNode->pRChild);
+  Visit(pNode);
+}
+
+int GetAllNodeNum(pBTNode &pNode)
+{
+  if(NULL == pNode)
+    return 0;
+  return GetAllNodeNum(pNode->pLChild)+GetAllNodeNum(pNode->pRChild)+1;
+}
+
+int GetBTDepthNum(pBTNode &pNode)
+{
+  if(NULL == pNode)
+    return 0;
+  int DepthLeftNum = GetBTDeepthNum(pNode->pLChild);
+  int DepthRightNum = GetBTDeepthNum(pNode->pRChild);
+  return DepthLeftNum > DepthRightNum ? DepthLeftNum+1 : DepthRightNum+1;
+}
+
+int GetLeafNodeNum(pBTNode &pNode)
+{
+  if(NULL == pNode)
+    return 0;  
+}
+
+int GetKthLevelNodeNum(pBTNode &pNode, int k)
+{
+  if(NULL == pNode)
+    return 0;
 }
